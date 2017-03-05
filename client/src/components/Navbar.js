ba06b2f2b9f723b32e9340092a58bd14d65004d6
krisaore <krisaore@gmail.com>
2017-03-05 16:27:57 +0100

    added new icon

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -11,6 +11,7 @@ class Navbar extends Component {
 				<div className="pull-right">
 					<div className="btn-toolbar" role="toolbar">
 						<div className="btn-group btn-group-lg" role="group">
+							<button type="button" className="btn btn-default navbar-btn" aria-label="New" title="New measure"><span className="fa fa-file-text-o" aria-hidden="true"></span></button>
 							<button type="button" className="btn btn-info navbar-btn" aria-label="Load" title="Load measure"><span className="fa fa-database" aria-hidden="true"></span></button>
 							<button type="button" className="btn btn-success navbar-btn" aria-label="Save" title="Save measure"><span className="fa fa-floppy-o" aria-hidden="true"></span></button>
 						</div>
